65fa3cfc49881b8b07d78f8d21d357e65614243c
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2018-08-12 13:10:09 +0200

    moved iline funcs to cpp

--- CaptureLoop.h
+++ CaptureLoop.h
@@ -33,39 +33,8 @@ private:
     time_t *_pUsedChannels;
     float *_pClippingChannels;
 
-    inline void checkConfig() {
-        //Check if new config file has been selected
-        const char input = Keyboard::getDigit();
-        if (input) {
-            throw ConfigChangedException(input);
-        }
-        //Check if config file on disk has changed
-        if (_pConfig->hasChanged()) {
-            throw ConfigChangedException();
-        }
-    }
-
-    inline void checkUsedChannels() {
-        const time_t now = Date::getCurrentTimeMillis();
-        //Compare now against last used timestamp and determine active channels
-        for (size_t i = 0; i < _nChannelsIn; ++i) {
-            if (now - _pUsedChannels[i] > 1000) {
-                _pUsedChannels[i] = 0;
-            }
-        }
-        //Update conditional routing
-        for (const Input *pInut : _inputs) {
-            pInut->evalConditions();
-        }
-    }
-
-    inline void checkClippingChannels() {
-        for (size_t i = 0; i < _nChannelsOut; ++i) {
-            if (_pClippingChannels[i] != 0) {
-                printf("WARNING: Output(%s) - Clipping detected: +%0.2f dBFS\n", _pConfig->getChannelName(i).c_str(), Convert::levelToDb(_pClippingChannels[i]));
-                _pClippingChannels[i] = 0;
-            }
-        }
-    }
+	void checkConfig();
+	void checkUsedChannels();
+	void checkClippingChannels();
 
 };

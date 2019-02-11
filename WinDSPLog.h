ff10ccea9fd9f475985a53eda636f106a6775fde
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2019-02-11 14:00:35 +0100

    Updated define for error log

--- WinDSPLog.h
+++ WinDSPLog.h
@@ -1,14 +1,15 @@
 #pragma once
-
-#define DEBUG_LOG
-
-#ifdef DEBUG_LOG
 #include "Log.h"
-#define LOG_FILE "WinDSP_log.txt"
+
+//#define DEBUG_LOG
+
 #undef LOG_INFO
 #undef LOG_WARN
 #undef LOG_ERROR
 #undef LOG_NL
+
+#ifdef DEBUG_LOG
+#define LOG_FILE "WinDSP_log.txt"
 #define LOG_INFO(str, ...)	printf(str,  ##__VA_ARGS__); printf("\n");	__LOG_INFO__(str,  ##__VA_ARGS__)
 #define LOG_WARN(str, ...)	printf(str,  ##__VA_ARGS__); printf("\n");	__LOG_WARN__(str,  ##__VA_ARGS__)
 #define LOG_ERROR(str, ...) printf(str,  ##__VA_ARGS__); printf("\n");	__LOG_ERROR__(str,  ##__VA_ARGS__)
@@ -16,6 +17,6 @@
 #else
 #define LOG_INFO(str, ...)	printf(str,  ##__VA_ARGS__); printf("\n")
 #define LOG_WARN LOG_INFO
-#define ERROR LOG_INFO
+#define LOG_ERROR LOG_INFO
 #define LOG_NL()			printf("\n");
 #endif

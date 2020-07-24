a2e29e1a17a636cdbc540a9bc0eb28441c2736d4
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2020-07-24 12:30:52 +0200

    Compression filter can no be used without a window

--- lib/DSP/src/FilterCompression.cpp
+++ lib/DSP/src/FilterCompression.cpp
@@ -5,12 +5,19 @@
 FilterCompression::FilterCompression(const uint32_t sampleRate, const double threshold, const double ratio,
     const double attack, const double release, const double window) {
     _threshold = Convert::dbToLevel(threshold);
-    _ratio = ratio;
-    _attackCoef = exp(-1000.0 / (attack * sampleRate));
-    _releaseCoef = exp(-1000.0 / (release * sampleRate));
-    _windowCoef = exp(-1000.0 / (window * sampleRate));
+    _ratio = ratio - 1;
     _squaredSum = 0;
     _envelope = 0;
+    _attackCoef = exp(-1000.0 / (attack * sampleRate));
+    _releaseCoef = exp(-1000.0 / (release * sampleRate));
+    if (window > 0) {
+        _useWindow = true;
+        _windowCoef = exp(-1000.0 / (window * sampleRate));
+    }
+    else {
+        _useWindow = false;
+        _windowCoef = 0;
+    }
 }
 
 const vector<string> FilterCompression::toString() const {
--- lib/DSP/src/FilterCompression.h
+++ lib/DSP/src/FilterCompression.h
@@ -20,16 +20,22 @@ public:
         const double ratio, // [0.0, 1.0] 0.0=oo:1, 0.5=2:1, 1.0=1:1
         const double attack, // ms
         const double release, // ms
-        const double window // ms
-        );
+        const double window = 0 // ms
+    );
 
     const vector<string> toString() const;
 
     inline const double process(const double sample) override {
-        const double sum = sample * sample;
-        run(sum, _windowCoef, _squaredSum);
-        const double rms = sqrt(_squaredSum);
-        double over = rms / _threshold;
+        double over;
+        //Use a sliding rms window to calculate how much the sample is over the threshold.
+        if (_useWindow) {
+            run(sample * sample, _windowCoef, _squaredSum);
+            over = sqrt(_squaredSum) / _threshold;
+        }
+        //Use just the single sample.
+        else {
+            over = sample * sample / _threshold;
+        }
         if (over < 1) {
             over = 1;
         }
@@ -41,14 +47,14 @@ public:
         else {
             run(over, _releaseCoef, _envelope);
         }
-        const double gain = over ? pow(over, _ratio - 1) : 1;
-        return sample * gain;
+        return sample * pow(over, _ratio);
     }
 
     inline void reset() override { }
 
 private:
     double  _threshold, _ratio, _attackCoef, _releaseCoef, _windowCoef, _envelope, _squaredSum;
+    double _useWindow;
 
     inline void run(const double in, const double coef, double& state) const {
         state = in + coef * (state - in);
